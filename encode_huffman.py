d834208f26d2668e545b72b540248598aaf561e7
lagovsp <lagovsp@gmail.com>
2023-03-10 22:54:41 +0300

    bugs fixed

--- encode_huffman.py
+++ encode_huffman.py
@@ -7,12 +7,12 @@ class HuffmanCoder:
         def __init__(self,
                      is_terminal: bool = False,
                      elem: str = None,
-                     lower_pred: 'HuffmanCoder.Vertex' = None,
-                     greater_pred: 'HuffmanCoder.Vertex' = None):
+                     low_pred: 'HuffmanCoder.Vertex' = None,
+                     great_pred: 'HuffmanCoder.Vertex' = None):
             self.is_terminal = is_terminal
             self.elem = elem
-            self.lower = lower_pred
-            self.greater = greater_pred
+            self.lower = low_pred
+            self.greater = great_pred
 
         def __repr__(self) -> str:
             if self.is_terminal:
@@ -20,48 +20,45 @@ class HuffmanCoder:
             return f'({self.lower} & {self.greater})'
 
     def __init__(self):
-        self.syms_entries = dict[Any, int]()
+        self.syms_entries = dict[HuffmanCoder.Vertex, int]()
         self.codes = dict[str, str]()
 
     def reset_dict(self) -> None:
         self.syms_entries.clear()
 
     def _create_dict(self, data: str):
+        syms_to_times = dict[str, int]()
+
         for s in data:
-            inserted = False
-            for key, val in self.syms_entries.copy().items():
-                if not key.is_terminal or not key.elem == s:
-                    continue
-                self.syms_entries[key] += 1
-                inserted = True
-                break
-            if not inserted:
-                self.syms_entries.update({HuffmanCoder.Vertex(is_terminal=True,
-                                                              elem=s): 1})
+            if s in syms_to_times:
+                syms_to_times[s] += 1
+                continue
+            syms_to_times.update({s: 1})
+
+        self.syms_entries = {
+            HuffmanCoder.Vertex(is_terminal=True, elem=key): val
+            for key, val in syms_to_times.items()
+        }
 
     @staticmethod
-    def _extract_min_vertex(src: dict[Any, int]) -> Any:
-        lower = min(src.items(), key=lambda x: x[1])
-        del src[lower[0]]
-        return lower
+    def _extract_min_vertex(vertexes: dict[Any, int]) -> ('HuffmanCoder.Vertex', int):
+        lower, times = min(vertexes.items(), key=lambda x: x[1])
+        del vertexes[lower]
+        return lower, times
 
     def _create_tree(self) -> 'HuffmanCoder.Vertex':
         s_entries = self.syms_entries.copy()
 
         if not s_entries:
-            return HuffmanCoder.Vertex(is_terminal=True)
-        if len(s_entries) == 1:
-            sym, entries = s_entries.popitem()[0]
-            self.codes.update({sym: entries})
-            return HuffmanCoder.Vertex(is_terminal=True)
+            raise Exception('NO DATA GIVEN')
 
         while not len(s_entries) == 1:
-            lower = HuffmanCoder._extract_min_vertex(s_entries)
-            greater = HuffmanCoder._extract_min_vertex(s_entries)
+            lower, l_times = HuffmanCoder._extract_min_vertex(s_entries)
+            greater, g_times = HuffmanCoder._extract_min_vertex(s_entries)
 
-            ver = HuffmanCoder.Vertex(lower_pred=lower[0],
-                                      greater_pred=greater[0])
-            s_entries.update({ver: lower[1] + greater[1]})
+            s_entries.update(
+                {HuffmanCoder.Vertex(low_pred=lower, great_pred=greater): l_times + g_times}
+            )
 
         return s_entries.popitem()[0]
 
@@ -70,20 +67,22 @@ class HuffmanCoder:
             self._set_codes_from_tree(ver.greater, prefix=prefix[:] + '1')
             self._set_codes_from_tree(ver.lower, prefix=prefix[:] + '0')
             return
-        self.codes.update({ver.elem: prefix})
+        self.codes.update({ver.elem: prefix if prefix else '0'})
 
     def encode(self, data: str) -> (list[str], dict[str, str]):
         self._create_dict(data)
-        top_ver = self._create_tree()
-        self._set_codes_from_tree(top_ver)
+        self._set_codes_from_tree(self._create_tree())
         return [self.codes[s] for s in data], self.codes
 
 
 def main():
+    if not len(sys.argv) == 2:
+        raise Exception(f'BAD ARGUMENTS')
+
     coder = HuffmanCoder()
     coded_data, codes = coder.encode(sys.argv[1])
-    print('USED FOLLOWING CODES', codes)
-    print('CODED DATA', ' '.join(coded_data))
+    print(f'USED FOLLOWING CODES\n{codes}')
+    print(f'CODED DATA\n{" ".join(coded_data)}')
 
 
 if __name__ == '__main__':
